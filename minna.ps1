592c94645f9701905a2fa5f62114603b9182bd69
rinta <120776920+rintaro-s@users.noreply.github.com>
2025-08-07 11:41:13 +0900

    Update minna.ps1

--- minna.ps1
+++ minna.ps1
@@ -1,11 +1,11 @@
 # 画像のURL
-$imageUrl = "https://scontent-itm1-1.xx.fbcdn.net/v/t39.30808-6/326570040_3110401969250133_2163029026329463223_n.png?_nc_cat=1&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=qZ6XQrwaOhwQ7kNvwELrfrF&_nc_oc=Adn5HfgA3hy4ceSDCgVQyv0ZrwODqztB-eJh-PkMvgiN_Rm96McL_MxpyVPjIELxM4-ZmD6vSIDqATBqJw4qAABr&_nc_zt=23&_nc_ht=scontent-itm1-1.xx&_nc_gid=SQFa9mcuNQi9ZoD1aIH5jQ&oh=00_AfVkzLVKhhtIv6ccLmQ2e2K646Je2kds-_KD0H9VzuuFNQ&oe=6899E5FC"
+$imageUrl = "https://scontent-itm1-1.xx.fbcdn.net/v/t39.30808-6/326570040_3110401969250133_2163029026329463223_n.png?_nc_cat=1&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=qZ6XQrwaOhwQ7kNvwELrfrH&_nc_oc=Adn5HfgA3hy4ceSDCgVQyv0ZrwODqztB-eJh-PkMvgiN_Rm96McL_MxpyVPjIELxM4-ZmD6vSIDqATBqJw4qAABr&_nc_zt=23&_nc_ht=scontent-itm1-1.xx&_nc_gid=SQFa9mcuNQi9ZoD1aIH5jQ&oh=00_AfVkzLVKhhtIv6ccLmQ2e2K646Je2kds-_KD0H9VzuuFNQ&oe=6899E5FC"
 
 # デスクトップのパス
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 
 # ダウンロード先のファイル名とパス
-$downloadPath = Join-Path -Path $desktopPath -ChildPath "minna no ginko.jpg"
+$downloadPath = Join-Path -Path $desktopPath -ChildPath "Minna-no-Ginko_original.png"
 
 #画像をダウンロード
 Write-Host "画像をダウンロードしています..."
@@ -20,7 +20,9 @@ try {
 # 画像を100個コピー
 Write-Host "画像を100個コピーしています..."
 for ($i = 1; $i -le 100; $i++) {
-    $copyPath = Join-Path -Path $desktopPath -ChildPath "Minna no ginko_$i.jpg"
+    # ファイル名にコピー番号を追加
+    $copyPath = Join-Path -Path $desktopPath -ChildPath "Minna-no-Ginko_$i.png"
+    
     Copy-Item -Path $downloadPath -Destination $copyPath -Force
 }
 
